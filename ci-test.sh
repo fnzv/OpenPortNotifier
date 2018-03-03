081614935c50a2664f2a8b093ff61262751f94b7
Sami <fnzv@users.noreply.github.com>
2018-03-03 15:06:23 +0100

    Update ci-test.sh

--- ci-test.sh
+++ ci-test.sh
@@ -1,15 +1,3 @@
-# Golang quick install
-
- apt-get update
- wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
- sudo tar -xvf go1.9.2.linux-amd64.tar.gz
- sudo mv go /usr/local
- echo "export GOROOT=/usr/local/go" >> /root/.bashrc
- echo "export GOPATH=$HOME/Projects" >> /root/.bashrc
- echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> /root/.bashrc
- echo "Checking Golang version\n"
- ln -s /usr/local/go/bin/go /usr/bin/go
-
 # OPN Installer
 
  go get ./src
